c46453b8790fd3682ee2bed9fe8f3b1a847e0124
adunn-kal <alexanderkdunn1@gmail.com>
2022-12-22 18:01:22 -0800

    made main file actually work

--- src/main.cpp
+++ src/main.cpp
@@ -14,29 +14,6 @@ int32_t distance = 500;
 float temp = 32.2;
 float hum = 45.5;
 
-UnixTime uTime = 1671221899;
-
-
-
-
-// Create a unix timestamp of the current time including milliseconds
-char* unixTime(UnixTime now)
-{
-    snprintf(format_buf, FORMAT_BUF_SIZE, "%d.%03d", now.getUnix(), (millis() - 500)%1000);
-    return format_buf;
-}
-
-// Return a current unix timestamp
-UnixTime getTime(void)
-{
-    UnixTime stamp(500);
-
-    // If the GPS has a fix, use it to create a timestamp
-    stamp.setDateTime(2000 + 22, 12, 16, 13, 35, 30);
-
-    return stamp;
-}
-
 
 
 void setup()
@@ -46,10 +23,11 @@ void setup()
   delay(2000);
   startTime = snprintf(format_buf, FORMAT_BUF_SIZE, "123.45");
 
-  myCard.writeHeader(startTime);
+  myCard.writeHeader();
   Serial.println("Done writing header file");
   delay(2000);
 
+  String uTime = "123";
   myFile = myCard.createFile(1, 30, uTime, 60*1000);
   Serial.println("Done creating file");
 
@@ -61,7 +39,8 @@ void loop()
   for (int i = 0; i < 10; i++)
   {
     delay(1000);
-    myCard.writeData(myFile, distance, unixTime(getTime()), temp, hum, 1);
+    String time = "123";
+    myCard.writeData(myFile, distance, time, temp, hum, 1);
     Serial.printf("Done writing file %d\n", i+1);
   }
   myFile.close();
